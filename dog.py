60d0beb5f05dce4f519b71c7a4652ab430667536
ochoa-josue <89376717+ochoa-josue@users.noreply.github.com>
2023-04-07 23:41:18 -0700

    Created dog.py

---
+++ dog.py
@@ -0,0 +1,15 @@
+from animal import Animal
+class Dog(Animal):
+    
+    def bark(self):
+        print("I can bark! Woof woof!!")
+
+# Create object of the Dog class
+dog1 = Dog()
+
+# Calling members of the base class
+dog1.eat()
+dog1.sleep()
+
+# Calling member of the derived class
+dog1.bark();
